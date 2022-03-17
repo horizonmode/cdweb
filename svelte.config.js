2ed26479946f1558c15795adfeaca08610fe30c4
ssmithwitekio <97116699+ssmithwitekio@users.noreply.github.com>
2022-03-17 08:09:49 +0000

    static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-// import adapter from '@sveltejs/adapter-auto';
-import azure from 'svelte-adapter-azure-swa';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: azure()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
-
-export default config;
